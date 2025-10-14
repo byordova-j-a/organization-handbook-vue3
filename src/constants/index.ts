85234ab6c72fa9e0d30a0e0a5191bdcc2f9a2eea
byordova-j-a <berd.rabbit@gmail.com>
2025-10-15 00:24:40 +0500

    feat(app): add build

--- src/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const PHONE_NUMBER_REGEX = /^\+7-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
-export const NUMBER_REGEX = /^[1-9]\d*$/;
-
-export const LEFT_ARROW = '<';
-export const RIGHT_ARROW = '>';
-export const CROSS = 'X';
